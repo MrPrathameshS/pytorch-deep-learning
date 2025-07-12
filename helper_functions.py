31ef06084b498f6d18e37c223a2141ccabd6f44a
MrPrathameshS <153704604+MrPrathameshS@users.noreply.github.com>
2025-07-12 11:58:29 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -292,3 +292,16 @@ def download_data(source: str,
             os.remove(data_path / target_file)
     
     return image_path
+
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+
